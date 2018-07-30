9cc27c4a21b99661f4a1e329b5067dc2ece1c3f7
Henry Zou <zouhenry@gmail.com>
2018-07-29 22:24:14 -0400

    updating port

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var path = require('path');
 var express = require('express');
 
+const PORT = process.env.PORT || 8080
+
 var app = express();
 
 var staticPath = path.join(__dirname, '/dist');
 app.use(express.static(staticPath));
 
-app.listen(80, function() {
-  console.log('listening on http://localhost:8080');
+app.listen(PORT, function () {
+  console.log('listening on http://localhost:' + PORT);
 });
